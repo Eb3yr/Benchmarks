7f429520d3b0c7815e72318a1633b8961d09c6a0
Eb3yr <Eb3yr123@gmail.com>
2025-02-23 08:06:50 +0000

    * Fix incorrect TensorPrimitives implementation
    * Add Sliding Window TensorPrimitives implementation to avoid heap allocations and speed up large Length runs
    
    * TODO: Investigate relationship between Sliding Window implementation, max stackalloc const, and how it compares to the vectorised implementation (the runner-up performance-wise for larger Length, and the dominant one of smaller Length)

--- TrapeziumRule/Benchmarks.cs
+++ TrapeziumRule/Benchmarks.cs
@@ -33,18 +33,6 @@ namespace TrapeziumRule
 			y = new T[Length];
 			for (int i = 0; i < x.Length; i++)
 				y[i] = F(x[i]);
-
-			T tLen = (T)Convert.ChangeType(Length, typeof(T));
-			T expected = tLen * tLen * tLen / (Two + T.One) + tLen;
-			T oneHundred = (T)Convert.ChangeType(100f, typeof(T));
-			Console.BackgroundColor = ConsoleColor.Green;
-			Console.WriteLine($"Length: {Length}\tExpected: {expected}. Actual: {Trapz()}, Ptr: {TrapzPtrUnrolled()}. Error: {oneHundred - oneHundred * Trapz() / expected}%");
-			if (TrapzPtrUnrolled() != TrapzVectors())
-			{
-				Console.WriteLine($"Mismatch: Analytical: {Length * Length * Length / 3 + Length}, TrapzPtr: {TrapzPtrUnrolled()}, Vectorised: {TrapzVectors()}");
-				Task.Delay(3000).Wait();
-			}
-			Console.BackgroundColor = ConsoleColor.Gray;
 		}
 
 		[Benchmark]
@@ -52,18 +40,18 @@ namespace TrapeziumRule
 		public T Trapz()
 		{
 			T result = T.Zero;
-
+		
 			for (int i = 0; i < x.Length - 1; i++)  // I could do this with IEnumerable because I'm just iterating each one
 				result += (y[i + 1] + y[i]) * (x[i + 1] - x[i]);  // 0.5(a+b)h
-
+		
 			return half * result;
 		}
-
+		
 		[Benchmark]
 		public unsafe T TrapzPtr()
 		{
 			T result = T.Zero;
-
+		
 			fixed (T* xPtr = x)
 			fixed (T* yPtr = y)
 			{
@@ -72,21 +60,21 @@ namespace TrapeziumRule
 					result += (yPtr[i + 1] + yPtr[i]) * (xPtr[i + 1] - xPtr[i++]);
 			}
 			result /= T.One + T.One;
-
+		
 			return result;
 		}
-
+		
 		[Benchmark(Baseline = true)]
 		public unsafe T TrapzPtrUnrolled()
 		{
 			T result = T.Zero;
-
+		
 			fixed (T* xPtr = x)
 			fixed (T* yPtr = y)
 			{
 				int i = 0;
 				int remainder = (x.Length - 1) / 4;
-
+		
 				while (remainder > 0)
 				{
 					result += (yPtr[i + 1] + yPtr[i]) * (xPtr[i + 1] - xPtr[i++]);
@@ -95,28 +83,59 @@ namespace TrapeziumRule
 					result += (yPtr[i + 1] + yPtr[i]) * (xPtr[i + 1] - xPtr[i++]);
 					remainder -= 4;
 				}
-
+		
 				while (i < x.Length - 1)
 					result += (yPtr[i + 1] + yPtr[i]) * (xPtr[i + 1] - xPtr[i++]);
 			}
 			result /= (T.One + T.One);
-
+		
 			return result;
 		}
 
 		[Benchmark]
 		public T TrapzTensorPrimitives()
 		{
-			T result = T.Zero;
-			Span<T> dest = x.Length <= 1024 ? stackalloc T[x.Length - 1] : new T[x.Length - 1];
-			Span<T> xDiff = x.Length <= 1024 ? stackalloc T[x.Length - 1] : new T[x.Length - 1];
+			Span<T> dest = x.Length <= 2048 ? stackalloc T[x.Length - 1] : new T[x.Length - 1];
+			Span<T> xDiff = x.Length <= 2048 ? stackalloc T[x.Length - 1] : new T[x.Length - 1];
 			Span<T> _x = x;
 			Span<T> _y = y;
 
-			TensorPrimitives.Subtract(_x.Slice(1), _x.Slice(0, _x.Length - 1), xDiff);
-			TensorPrimitives.Subtract(_y.Slice(1), _y.Slice(0, _x.Length - 1), dest);
+			TensorPrimitives.Subtract(_x.Slice(1, _x.Length - 1), _x.Slice(0, _x.Length - 1), xDiff);
+			TensorPrimitives.Add(_y.Slice(1, _x.Length - 1), _y.Slice(0, _x.Length - 1), dest);
 			TensorPrimitives.Multiply(dest, xDiff, dest);
 			return TensorPrimitives.Sum<T>(dest) / (T.One + T.One);
+		}
+
+		[Benchmark]
+		public T TrapzTensorPrimitivesSlidingWindow()
+		{
+			const int stackLim = 1024;	// 2^n + 1 to keep dest, xDiff lengths powers of 2. 
+			int remainder = x.Length;
+			T result = T.Zero;
+			int minOfLength = int.Min(x.Length - 1, stackLim);
+			Span<T> dest = stackalloc T[minOfLength];
+			Span<T> xDiff = stackalloc T[minOfLength];
+			Span<T> _x = x;
+			Span<T> _y = y;
+
+			int offset = 0;
+			while (remainder > stackLim)
+			{
+				TensorPrimitives.Subtract(_x.Slice(1 + offset, stackLim), _x.Slice(offset, stackLim), xDiff);
+				TensorPrimitives.Add(_y.Slice(1 + offset, stackLim), _y.Slice(offset, stackLim), dest);
+				TensorPrimitives.Multiply(dest, xDiff, dest);
+				result += TensorPrimitives.Sum<T>(dest);
+
+				offset += stackLim;
+				remainder -= stackLim;
+			}
+
+			// We slice the destinations instead of re-allocating them, so that we don't have mismatched span lengths
+			TensorPrimitives.Subtract(_x.Slice(1 + offset, remainder - 1), _x.Slice(offset, remainder - 1), xDiff.Slice(0, remainder - 1));
+			TensorPrimitives.Add(_y.Slice(1 + offset, remainder - 1), _y.Slice(offset, remainder - 1), dest.Slice(0, remainder - 1));
+			TensorPrimitives.Multiply(dest.Slice(0, remainder - 1), xDiff.Slice(0, remainder - 1), dest.Slice(0, remainder - 1));
+			result += TensorPrimitives.Sum<T>(dest.Slice(0, remainder - 1));
+			return result / Two;
 		}
 
 		[Benchmark]
