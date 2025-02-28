00e905487cb1fe21b9db61798cf05b537c8be02a
Eb3yr <Eb3yr123@gmail.com>
2025-02-28 18:55:58 +0000

    All benchmark methods

--- ArraySum/Benchmarks.cs
+++ ArraySum/Benchmarks.cs
@@ -16,7 +16,7 @@ namespace ArraySum
 	{
 		float[] vals;
 
-		[Params(8, 128, 4096, 65536/*, 1048576*/)]
+		[Params(4, 16, 128, 4096, 65536)]
 		public int Length;
 
 		[GlobalSetup]
@@ -26,9 +26,18 @@ namespace ArraySum
 			Random rng = new(0);
 			for (int i = 0; i < vals.Length; i++)
 				vals[i] = rng.NextSingle();
+
+			Debug.Assert(SumPointers() == SumFor());
+			Debug.Assert(SumPointersUnwound() == SumFor());
 		}
 
 		[Benchmark]
+		public float SumLinq()
+		{
+			return vals.Sum();
+		}
+
+		[Benchmark(Baseline = true)]
 		public float SumFor()
 		{
 			float sum = 0;
@@ -37,14 +46,53 @@ namespace ArraySum
 		
 			return sum;
 		}
-		
-		//[Benchmark]
-		//public float SumLinq()	// Slower than SumFor()
-		//{
-		//	return vals.Sum();
-		//}
 
-		[Benchmark(Baseline = true)]
+		[Benchmark]
+		public float SumForeach()
+		{
+			float sum = 0;
+			foreach (float f in vals)
+				sum += f;
+
+			return sum;
+		}
+
+		[Benchmark]
+		public unsafe float SumPointers()
+		{
+			float sum = 0f;
+			fixed (float* ptr = vals)
+			{
+				for (int i = 0; i < vals.Length; i++)
+					sum += ptr[i];
+			}
+			return sum;
+		}
+
+		[Benchmark]
+		public unsafe float SumPointersUnwound()
+		{
+			float sum = 0f;
+			fixed (float* ptr = vals)
+			{
+				int remainder = vals.Length / 4;
+				int i = 0;
+				while (remainder > 0)
+				{
+					sum += ptr[i++];
+					sum += ptr[i++];
+					sum += ptr[i++];
+					sum += ptr[i++];
+					remainder -= 4;
+				}
+
+				for (; i < vals.Length; i++)
+					sum += ptr[i];
+			}
+			return sum;
+		}
+
+		[Benchmark]
 		public float SumVector()
 		{
 			ReadOnlySpan<float> vals = new(this.vals);
@@ -55,32 +103,10 @@ namespace ArraySum
 				sum += Vector.Sum(new Vector<float>(vals.Slice(i, Vector<float>.Count)));
 				i += Vector<float>.Count;
 			}
-
+		
 			foreach (float f in vals.Slice(i))
 				sum += f;
-
-			return sum;
-		}
-
-		[Benchmark]
-		public unsafe float SumVector256()
-		{
-			ReadOnlySpan<float> vals = new(this.vals);
-			float sum = 0;
-
-			int i = 0;
-			fixed (float* ptr = vals)
-			{
-				while (i < vals.Length - Vector256<float>.Count)
-				{
-					sum += Vector256.Sum(Vector256.Load(ptr + i));
-					i += Vector256<float>.Count;
-				}
-			}
-
-			foreach (float f in vals.Slice(i))
-				sum += f;
-
+		
 			return sum;
 		}
 
@@ -89,14 +115,6 @@ namespace ArraySum
 		{
 			ReadOnlySpan<float> vals = new(this.vals);
 			return TensorPrimitives.Sum(vals);
-		}
-
-		[Benchmark]
-		public float SumTensor()
-		{
-#pragma warning disable SYSLIB5001
-			ReadOnlyTensorSpan<float> vals = new(this.vals);
-			return Tensor.Sum(vals);
 		}
 	}
 }
